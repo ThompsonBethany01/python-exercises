40906c492daf7b742ca0fedaa3f4fb0ff18c4bab
Bethany Thompson <thompson.bethany.01@gmail.com>
2020-08-05 22:46:06 -0500

    Adds Exercise - Needs Commenting

---
+++ import_exercises.py
@@ -0,0 +1,15 @@
+'''
+Import and test 3 of the functions from your functions exercise file.
+
+Import each function in a different way:
+
+import the module and refer to the function with the . syntax
+use from to import the function directly
+use from and give the function a different name
+'''
+
+import function_exercises
+#outputs True
+print((function_exercises.is_two(2))
+
+import function_exercises as fe
